72db956c8f06c04ab027c29e93921bc345087805
Miko <mikohendroc@gmail.com>
2017-10-21 11:04:10 +0700

    :123

--- app/src/main/java/com/example/it01/sistempakar/activity/konsultasi/Kesimpulan.java
+++ app/src/main/java/com/example/it01/sistempakar/activity/konsultasi/Kesimpulan.java
@@ -4,32 +4,46 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
 
 import com.example.it01.sistempakar.R;
 
-import org.w3c.dom.Text;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import me.anwarshahriar.calligrapher.Calligrapher;
 
 public class Kesimpulan extends AppCompatActivity {
     SharedPreferences sharedPreferences;
-    @BindView(R.id.g01)
-    TextView g01;
-    @BindView(R.id.g02)
-    TextView g02;
+//    @BindView(R.id.g01)
+//    TextView g01;
+//    @BindView(R.id.g02)
+//    TextView g02;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.list_view)
+    ListView list_view;
+    private String[] kodeGejala;
+    private String[] namaGejala;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kesimpulan);
         ButterKnife.bind(this);
+        Calligrapher calligrapher = new Calligrapher(this);
+        calligrapher.setFont(this, "font/art_rewery.ttf", true);
         sharedPreferences = getApplicationContext().getSharedPreferences(getResources().getString(R.string.app_pertanyaan), Context.MODE_PRIVATE);
         int gejala1 = sharedPreferences.getInt("G01", 9999);
         int gejala2 = sharedPreferences.getInt("G02", 9999);
-        g01.setText("mual : " + gejala1 + "");
-        g02.setText("demam" + gejala2 + "");
+//        g01.setText("mual : " + gejala1 + "");
+//        g02.setText("demam" + gejala2 + "");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Kesimpulan");
+        kodeGejala = new String[2];
+        namaGejala = new String[2];
+
     }
 }
---
+++ app/src/main/java/com/example/it01/sistempakar/adapter/KesimpulanAdapter.java
@@ -0,0 +1,31 @@
+package com.example.it01.sistempakar.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by IT01 on 10/21/2017.
+ */
+
+public class KesimpulanAdapter extends BaseAdapter{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
