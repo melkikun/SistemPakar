e9460df8922f200d0833399aaf9cc28662e6be56
Miko <mikohendroc@gmail.com>
2017-10-23 09:29:20 +0700

    123

---
+++ app/src/main/java/com/example/it01/sistempakar/activity/konsultasi/G17.java
@@ -0,0 +1,15 @@
+package com.example.it01.sistempakar.activity.konsultasi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.it01.sistempakar.R;
+
+public class G17 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_g03);
+    }
+}
